b1c509e60b24c8615208df2d4702e7c52eca7d76
lee <lsljw65@gmail.com>
2024-06-10 21:01:53 +0900

    table 정리

--- src/component/main/Post.js
+++ src/component/main/Post.js
@@ -1,12 +1,17 @@
-import '../../css/main/Post.css'
+import "../../css/main/Post.css";
 
-function Post(props){
-    return(
-        <div id='post'>
-            <span>{props.id}</span>
-            <span>{props.name}</span>
-        </div>
-    )
+function Post(props) {
+  return (
+    <tr id="post">
+      <td>{props.id}</td>
+      <td>{props.name}</td>
+      <td>{props.pd}</td>
+      <td>{props.au_num}</td>
+
+      {/* <span>{props.id}</span>
+            <span>{props.name}</span> */}
+    </tr>
+  );
 }
 
 export default Post;
--- src/component/main/PostList.js
+++ src/component/main/PostList.js
@@ -1,18 +1,25 @@
-import '../../css/main/PostList.css'
-import Post from './Post';
+import "../../css/main/PostList.css";
+import Post from "./Post";
 
-function PostList(props){
+function PostList(props) {
+  const result = props.movieList.map((data) => (
+    <Post id={data.id} name={data.name} pd={data.pd} au_num={data.au_num} />
+  ));
 
-    const result=props.movieList.map(
-        (data)=>(<Post id={data.id} name={data.name}/>)
-    )
-
-    return(
-        <div id='post-list'>
-            <h3>목록 리스트</h3>
-            {result}
-        </div>
-    )
+  return (
+    <div id="post-list">
+      <h3>목록 리스트</h3>
+      <table>
+        <tr>
+          <td>번호</td>
+          <td>제목</td>
+          <td>감독</td>
+          <td>관객수</td>
+        </tr>
+        {result}
+      </table>
+    </div>
+  );
 }
 
 export default PostList;
